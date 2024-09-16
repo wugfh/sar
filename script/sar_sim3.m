6f1782a985010010408c94f22384d3a4dbddbff0
aw <2759154365@qq.com>
2024-09-16 18:26:51 +0800

    add new data

--- script/sar_sim3.m
+++ script/sar_sim3.m
@@ -1,6 +1,6 @@
 clear,clc;
 
-load("../data/RadarSAT数据/RadarSAT数据/data_1.mat");
+load("../data/Stanley_Park_and_city/data_1.mat");
 
 c = 299792458;                     %光速
 Fs = 32317000;      %采样率                                   
@@ -11,15 +11,22 @@ PRF = 1.256980000000000e+03;       %PRF
 Vr = 7062;                       %雷达速度     
 B = 30.111e+06;        %信号带宽
 fc = -6900;          %多普勒中心频率
-Ka = 1733;
 Fa = PRF;
+% R0 = 9.886474620000000e+05;
+Ka = 1733;
 
 lambda = c/f0;
 Kr = -B/Tr;
+
 [Na, Nr] = size(data_1);
+data = zeros(Na, Nr+500);
+data(1:Na, 1:Nr) = data_1;
+[Na, Nr] = size(data);
+
+
 theta_rc = asin(fc*lambda/(2*Vr));
-R_eta_c = 2*Vr^2*cos(theta_rc)^2/(lambda*Ka);
-R0 = R_eta_c*cos(theta_rc);
+R0 = 2*Vr^2*cos(theta_rc)^3/(lambda*Ka);
+R_eta_c = R0/cos(theta_rc);
 eta_c = 2*Vr*sin(theta_rc)/lambda;
 
 f_tau = (-Nr/2:Nr/2-1)*(Fs/Nr);
@@ -27,6 +34,7 @@ f_eta = fc + (-Na/2:Na/2-1)*(Fa/Na);
 
 tau = 2*R_eta_c/c + (-Nr/2:Nr/2-1)*(1/Fs);
 eta = eta_c + (-Na/2:Na/2-1)*(1/Fa);
+
 
 [Ext_time_tau_r, Ext_time_eta_a] = meshgrid(tau, eta);
 [Ext_f_tau, Ext_f_eta] = meshgrid(f_tau, f_eta);
@@ -44,7 +52,7 @@ data_fft_a = fftshift(fft(fftshift(data_cr, 1), Na, 1), 1);
 
 data_fft_a_rcmc = data_fft_a;
 IN_N = 8;
-R0_RCMC = tau*c/2;  % 每个距离门都是一个R0
+R0_RCMC = tau*c/2;  
 delta_R = lambda^2*f_eta'.^2.*R0_RCMC/(8*Vr^2);
 delta_R_cnt = delta_R*2/(c*(1/Fs));
 for j = 1:Na
@@ -53,9 +61,9 @@ for j = 1:Na
         dR = delta_R_cnt(j,k);
         for m = -IN_N/2:IN_N/2-1
             if(k+floor(dR)+m>=Nr)
-                data_fft_a_rcmc(j,k) = data_fft_a_rcmc(j,k)+data_fft_a(j,Nr)*sinc(dR-(Nr-k));
+                continue;
             elseif(k+floor(dR)+m<=1)
-                data_fft_a_rcmc(j,k) = data_fft_a_rcmc(j,k)+data_fft_a(j,1)*sinc(dR-(1-k));
+                continue;
             else
                 data_fft_a_rcmc(j,k) = data_fft_a_rcmc(j,k)+data_fft_a(j,k+floor(dR)+m)*sinc(dR-floor(dR)-m);
             end
@@ -64,12 +72,12 @@ for j = 1:Na
 end
 
 % 方位压缩
-Ha = exp(-1j*pi*Ext_f_eta.^2/Ka);
-Offset = exp(-1j*2*pi*Ext_f_eta.*eta_c);
-data_fft_ca_rcmc = data_fft_a_rcmc.*Ha.*Offset;
+Ha = exp(-1j*pi*Ext_f_eta.^2./Ka);
+offset = exp(1j*2*pi*Ext_f_eta.*eta_c);
+data_fft_ca_rcmc = data_fft_a_rcmc.*Ha.*offset;
 data_ca_rcmc = fftshift(ifft(fftshift(data_fft_ca_rcmc, 1), Na, 1), 1);
 
-data_fft_ca = data_fft_a.*Ha.*Offset;
+data_fft_ca = data_fft_a.*Ha.*offset;
 data_ca = fftshift(ifft(fftshift(data_fft_ca, 1), Na, 1), 1);
 
 data_ca = 20*log(abs(data_ca)+1);
@@ -85,9 +93,9 @@ title("处理后的数据");
 
 figure;
 subplot(121)
-imagesc(data_ca);
+imagesc(mat2gray(data_ca));
 title("无rcmc");
 subplot(122)
-imagesc(data_ca_rcmc);
+imagesc(mat2gray(data_ca_rcmc));
 title("有rcmc");
 
