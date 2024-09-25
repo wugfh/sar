0f9755a0bff209c7dbe628efd6d70decd4ee6d1c
aw <2759154365@qq.com>
2024-09-25 22:05:05 +0800

    change subsequence

--- script/subband/subband1.m
+++ script/subband/subband1.m
@@ -11,7 +11,8 @@ Br = 2.8e7; % 子带带宽
 Fr = 1.2*Br; % 子带采样率
 step_f = Br;
 sub_N = 3;
-sub_f = f0:step_f:f0+step_f*sub_N;
+sub_f = f0+(1:sub_N)*step_f;
+f0 = sub_f((sub_N+1)/2);
 
 % sub_f = [sub_f2];
 c = 299792458;
@@ -24,7 +25,7 @@ incidence = deg2rad(20.5); % 入射角
 
 Kr = Br/Tr;
 sub_lambda = c./sub_f;
-lambda = c/(f0+step_f*sub_N/2);
+lambda = c/(f0);
 
 
 R0 = H/cos(phi);
@@ -84,10 +85,10 @@ for i = 1:sub_N
 
     Wr = abs(mat_t_tau_noisy-2*R_eta_c/c) < Tr/2;
     % Wa = sinc((La*atan(Vg*(mat_t_eta_offset - t_etac_target)./R0_target)/sub_lambda(i))).^2;
-    Wa = abs(mat_t_eta_offset - eta_c)<Ta/2;
-    phase = exp(1j*pi*Kr*(mat_t_tau_noisy-2*R_eta_target/c).^2).*exp(-2j*pi*sub_f(i)*2*R_eta_target/c).*exp(2j*pi*noisy_P(i));
+    Wa = abs(mat_t_eta_offset - eta_c - sub_t_offset(i))<Ta/2;
+    sub_phase = exp(1j*pi*Kr*(mat_t_tau_noisy-2*R_eta_target/c).^2).*exp(-2j*pi*sub_f(i)*2*R_eta_target/c).*exp(2j*pi*noisy_P(i));
 
-    S_echo(i,:,:) = (1+noisy_A(i))*Wr.*Wa.*phase;
+    S_echo(i,:,:) = (1+noisy_A(i))*Wr.*Wa.*sub_phase;
 end
 
 
@@ -111,113 +112,53 @@ f_tau_upsample = fftshift((-Nr_up/2:Nr_up/2-1)*(Fr/Nr));
 S_ftau_eta = zeros(Na, Nr_up);
 plot_range = 1:Nr;
 
-R_ref = sqrt(R0_target^2+(point(2)-Vr*(mat_t_eta_upsample+sub_t_offset((sub_N+1)/2))).^2);
+figure("name", "子带成像");
+
+target_upsample = zeros(3, Na, Nr_up);
+
 for i = 1:sub_N
     sub_S_echo = squeeze(S_echo(i, :, :));
+    sub_S_echo = sub_S_echo.*exp(2j*pi*sub_f(i)*2*R_eta_target/c);
     Hr =  (abs(mat_f_tau)<Br/2).*exp(1j*pi*mat_f_tau.^2/Kr);
     sub_S_ftau_eta = fft(sub_S_echo, Nr, 2);
     sub_S_ftau_eta = sub_S_ftau_eta.*Hr;
-    % 频域后置补零，升采样
-    tmp = zeros(Na, Nr_up);
-    tmp(:, (Nr_up/2):(Nr+Nr_up/2-1)) = fftshift(sub_S_ftau_eta, 2); %对准零频
-    % tmp = fftshift(tmp, 2); % 对准f_tau_upsample 频率轴，它的零频在两端
-    sub_S_tau_eta = ifft(tmp, Nr_up, 2); 
-    % 子带合成
-    % imagesc(abs(tmp));
-    sub_S_tau_eta = sub_S_tau_eta.*exp(-1j*2*pi*(i-(sub_N+1)/2)*step_f.*(mat_t_tau_upsample)); % 搬移子带频谱
-    sub_S_ftau_eta = fft(sub_S_tau_eta, Nr_up, 2);
+    sub_S_tau_eta = ifft(sub_S_ftau_eta, Nr, 2);
 
-    R_eta_target = sqrt(R0_target^2+(point(2)-Vr*mat_t_eta_upsample).^2);
-    sub_S_ftau_eta = sub_S_ftau_eta.*exp(2j*pi*(i-(sub_N+1)/2)*step_f*2*R_eta_target/c); %补偿跳变相位，此处相位来源于载频不同
+    sub_S_tau_feta = fft(sub_S_tau_eta, Na, 1);
+    delta_R = lambda^2*R0*mat_f_eta.^2/(8*Vr^2);
+    G_rcmc = exp(1j*4*pi*mat_f_tau.*delta_R/c);
+    sub_S_ftau_feta = fft(sub_S_tau_feta, Nr, 2);
+    sub_S_ftau_feta = sub_S_ftau_feta.*G_rcmc;
+    sub_S_tau_feta_rcmc = ifft(sub_S_ftau_feta, Nr, 2);
 
-    % 补偿方位向时延导致的斜距误差相位
-    R1_eta = sqrt(R0_target^2+(point(2)-Vr*(mat_t_eta_upsample+sub_t_offset(i))).^2);
-    sub_S_ftau_eta = sub_S_ftau_eta.*exp(2j*pi*2*(R1_eta-R_ref)/lambda);
+    mat_R0 = (mat_t_tau*c/2)*cos(theta_rc);
+    Ka = 2 * Vr^2 * cos(theta_rc)^2 ./ (lambda * mat_R0);
+    sub_S_tau_feta = sub_S_tau_feta_rcmc;
+    Ha = exp(-1j*pi*mat_f_eta.^2./Ka);
+    offset = exp(-1j*2*pi*mat_f_eta.*eta_c);
+    sub_S_tau_feta = sub_S_tau_feta.*Ha.*offset;
+    target = ifft(sub_S_tau_feta, Na, 1);
 
-    %补偿方位向时延
-    sub_S_ftau_feta = fft(sub_S_ftau_eta, Na, 1);
-    sub_S_ftau_feta = sub_S_ftau_feta.*exp(-2j*pi*(i-(sub_N+1)/2)*step_T.*mat_f_eta_upsample); 
-    sub_S_ftau_eta = ifft(sub_S_ftau_feta, Na, 1);
-
-    %合成
-    S_ftau_eta = S_ftau_eta+sub_S_ftau_eta;
+    sub_S_ftau_eta = fft(target, Nr, 2);
+    s_f_upsample = zeros(Na, Nr_up);
+    s_f_upsample(:,((Nr_up/2-Nr/2):(Nr_up/2+Nr/2-1))) = sub_S_ftau_eta;
+    target_upsample(i, :, :) = ifft(s_f_upsample, Nr_up, 2);
+    subplot(1,sub_N,i);
+    imagesc(abs(target));
 end
 
-figure("name", "脉压对比");
-S_tau_eta = ifft(S_ftau_eta, Nr_up, 2);
-range = 1:Nr_up;
-S_tau_eta_db = 20*log10(abs(S_tau_eta(round(Na/2),range)));
-S_tau_eta_db = (S_tau_eta_db-min(S_tau_eta_db))/(max(S_tau_eta_db)-min(S_tau_eta_db));
-sub_S_tau_eta_db = 20*log10(abs(sub_S_tau_eta(round(Na/2),range)));
-sub_S_tau_eta_db = (sub_S_tau_eta_db-min(sub_S_tau_eta_db))/(max(sub_S_tau_eta_db)-min(sub_S_tau_eta_db));
-plot(range, S_tau_eta_db);
-hold on;
-plot(range, sub_S_tau_eta_db);
-legend("合成带","子带");
+S_tau_eta = zeros(Na, Nr_up);
+R_ref = sqrt(R0_target^2+(point(2)-Vr*(mat_t_eta_upsample+sub_t_offset((sub_N+1)/2))).^2);
+for i = 1:sub_N
+    target = squeeze(target_upsample(i, :, :));
+    R_eta_target = sqrt(R0_target^2+(point(2)-Vr*(mat_t_eta_upsample+sub_t_offset(i))).^2);
+    target = target.*exp(1j*pi*Kr*(2*(-R_ref+R_eta_target)/c).*(2*mat_t_tau_upsample-2*(R_ref+R_eta_target)/c));
+    target_shift = target.*exp(2j*pi*(i-(sub_N+1)/2)*step_f.*mat_t_tau_upsample);
+    S_tau_eta = target_shift+S_tau_eta;
+end
 
-figure("name", "合成后的频带");
-subplot(1,2,1)
-plot(1:Nr_up, abs(S_ftau_eta(round(Na/2),:)));
-title("模");
-subplot(1,2,2)
-plot(1:Nr_up, angle(S_ftau_eta(round(Na/2),:)));
-title("相位");
-% imagesc(abs(S_ftau_eta));
-
-
-figure("name", "距离压缩");
+figure("name","最终效果");
 imagesc(abs(S_tau_eta));
-
-% Time_tau_Point =  round(((2*R0_target)/(c*cos(theta_rc))-t_tau(1))/(1/(Fr*sub_N)));%目标的距离向标准坐标(距离门)
-% Time_eta_Point = round(Na / 2 + (point(2) / Vr) /(1/PRF));
-% fprintf("点数坐标应为%d行(方),%d列(距)\n\n",Time_eta_Point,Time_tau_Point);
-
-%% RCMC
-% S_tau_feta = fft(S_tau_eta, Na, 1);
-% S_tau_feta_rcmc = S_tau_feta;
-% IN_N = 8;
-% R0_RCMC = t_tau_upsample*c/2;  
-% delta_R = lambda^2*f_eta'.^2.*R0_RCMC/(8*Vr^2);
-% delta_R_cnt = delta_R*2/(c*(1/Fr));
-% for j = 1:Na
-%     for k = 1:Nr_up
-%         dR = delta_R_cnt(j,k);
-%         pos = dR-floor(dR)-(-IN_N/2:IN_N/2-1);
-%         rcmc_sinc = sinc(pos);
-%         size_sinc = size(rcmc_sinc);
-%         predict_value = zeros(size_sinc);
-%         for m = -IN_N/2:IN_N/2-1
-%             if(k+floor(dR)+m>Nr_up)
-%                 predict_value(m+IN_N/2+1) = S_tau_feta(j,k+floor(dR)+m-Nr_up);
-%             else
-%                 predict_value(m+IN_N/2+1) = S_tau_feta(j,k+floor(dR)+m);
-%             end
-%         end
-%         S_tau_feta_rcmc(j,k) = sum(predict_value.*rcmc_sinc);
-%     end
-% end
-
-S_tau_feta = fft(S_tau_eta, Na, 1);
-delta_R = lambda^2*R0*mat_f_eta_upsample.^2/(8*Vr^2);
-G_rcmc = exp(1j*4*pi*mat_f_tau_upsample.*delta_R/c);
-S_ftau_feta = fft(S_tau_feta, Nr_up, 2);
-S_ftau_feta = S_ftau_feta.*G_rcmc;
-S_tau_feta_rcmc = ifft(S_ftau_feta, Nr_up, 2);
-
-figure("name", "rcmc");
-imagesc(abs(ifft(S_tau_feta_rcmc, Na, 1)));
-
-%% 方位压缩
-mat_R0 = (mat_t_tau_upsample*c/2)*cos(theta_rc);
-Ka = 2 * Vr^2 * cos(theta_rc)^2 ./ (lambda * mat_R0);
-S_tau_feta = S_tau_feta_rcmc;
-Ha = exp(-1j*pi*mat_f_eta_upsample.^2./Ka);
-offset = exp(-1j*2*pi*mat_f_eta_upsample.*eta_c);
-S_tau_feta = S_tau_feta.*Ha.*offset;
-target = ifft(S_tau_feta, Na, 1);
-
-figure("name", "目标");
-imagesc(abs(target));
 
 
 
