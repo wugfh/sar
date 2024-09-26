711536ca409e5472ce51d8d2b4eb085f6c838cb9
aw <2759154365@qq.com>
2024-09-26 20:27:26 +0800

    update

--- script/subband/subband1.m
+++ script/subband/subband1.m
@@ -4,11 +4,11 @@ clear; clc;
 EarthMass = 6e24; %地球质量(kg)
 EarthRadius = 6.37e6; %地球半径6371km
 Gravitational = 6.67e-11; %万有引力常量
-f0 = 5.4e9; % 中心频率
-H = 755e3; % 飞行高度
-Tr = 10e-6; % 脉冲宽度
-Br = 2.8e7; % 子带带宽
-Fr = 1.2*Br; % 子带采样率
+f0 = 347.5e6; % 中心频率
+H = 70520; % 飞行高度
+Tr = 50e-6; % 脉冲宽度
+Br = 15e6; % 子带带宽
+Fr = 17.5e6; % 子带采样率
 step_f = Br;
 sub_N = 3;
 sub_f = f0+(1:sub_N)*step_f;
@@ -16,10 +16,10 @@ f0 = sub_f((sub_N+1)/2);
 
 % sub_f = [sub_f2];
 c = 299792458;
-Vr = sqrt(Gravitational*EarthMass/(EarthRadius   + H));
+Vr = 124;
 Vg = Vr;
 Vs = Vr;
-La = 15;
+La = 2*c/(sub_N*Br);
 phi = deg2rad(20); % 俯仰角
 incidence = deg2rad(20.5); % 入射角
 
@@ -55,12 +55,12 @@ f_eta = fnc+(-Na/2:Na/2-1)*(PRF/Na);
 [mat_f_tau, mat_f_eta] = meshgrid(f_tau, f_eta);
 
 % 脉内串发，构造偏移时间
-step_T = Tr+Tr;
+step_T = 1/PRF;
 sub_t_offset = (0:sub_N-1)*step_T; % 添加保护带，偏移时间为2*T
 
 %% 点目标回波产生
-pointx = 500;
-pointy = 500;
+pointx = 0;
+pointy = 0;
 point = [pointx, pointy];
 
 
@@ -104,7 +104,7 @@ end
 
 %% 成像
 
-uprate = sub_N*8;
+uprate = sub_N;
 Nr_up = Nr*uprate;
 Na_up = Na*sub_N;
 t_tau_upsample = 2*R_eta_c/c + (-Nr_up/2:Nr_up/2-1)*(1/(Fr*uprate));
@@ -116,23 +116,28 @@ S_ftau_eta = zeros(Na, Nr_up);
 figure("name", "子带成像");
 
 target_upsample = zeros(3, Na, Nr_up);
-
+R_ref = sqrt(R0_target^2+(point(2)-Vr*(mat_t_eta+sub_t_offset(((sub_N+1)/2)))).^2);
 for i = 1:sub_N
     sub_S_echo = squeeze(S_echo(i, :, :));
     Hr =  (abs(mat_f_tau)<Br/2).*exp(1j*pi*mat_f_tau.^2/Kr);
     sub_S_ftau_eta = fft(sub_S_echo, Nr, 2);
     sub_S_ftau_eta = sub_S_ftau_eta.*Hr;
+
+    R_eta = sqrt(R0_target^2+(point(2)-Vr*(mat_t_eta+sub_t_offset(i))).^2);
+    sub_S_ftau_eta = sub_S_ftau_eta.*exp(-2j*pi*2*(R_eta-R_ref)/c.*mat_f_tau);
+
     sub_S_tau_eta = ifft(sub_S_ftau_eta, Nr, 2);
 
     sub_S_tau_eta = sub_S_tau_eta.*exp(2j*pi*(fnc-sub_fnc(i)).*mat_t_eta); %移动多普勒中心
-    % R_eta = sqrt(R0^2+(Vr*mat_t_eta_offset).^2);
-    % sub_S_tau_eta = sub_S_tau_eta.*exp(2j*pi*sub_f(i)*2*R_eta/c).*exp(-2j*pi*f0*2*R_eta/c);
 
     sub_S_tau_feta = fft(sub_S_tau_eta, Na, 1);
+    sub_S_tau_feta = sub_S_tau_feta.*exp(-2j*pi*mat_f_eta*sub_t_offset(i)); %方位向补偿时延
+
     mat_f_eta_offset = mat_f_eta;
     delta_R = lambda^2*R0*mat_f_eta_offset.^2/(8*Vr^2);
     G_rcmc = exp(1j*4*pi*mat_f_tau.*delta_R/c);
     sub_S_ftau_feta = fft(sub_S_tau_feta, Nr, 2);
+
     sub_S_ftau_feta = sub_S_ftau_feta.*G_rcmc;
     sub_S_tau_feta_rcmc = ifft(sub_S_ftau_feta, Nr, 2);
 
@@ -157,12 +162,7 @@ S_tau_eta = zeros(Na, Nr_up);
 echo_ref = squeeze(target_upsample((sub_N+1)/2, :, :));
 for i = 1:sub_N
     target = squeeze(target_upsample(i, :, :));
-    if(i ~= (sub_N+1)/2)
-        R_tar = sqrt(R0^2+(Vr*(mat_t_eta_upsample+sub_t_offset(i))).^2);
-        tar_ftau_eta = fft(target, Nr_up, 2);
-        tar_ftau_eta = tar_ftau_eta.*exp(-2j*pi*2*(R_tar-R_ref)/c.*mat_f_tau_upsample);
-        target = ifft(tar_ftau_eta, Nr_up, 2);
-    end    
+ 
     target_shift = target.*exp(2j*pi*(i-(sub_N+1)/2)*step_f.*mat_t_tau_upsample);
     S_tau_eta = target_shift+S_tau_eta;
 end
@@ -175,7 +175,7 @@ target_one = squeeze(target_upsample(1, :,:));
 find_tmp = target_one';
 [max_value, a_pos] = max(max(find_tmp));
 [max_value, r_pos] = max(max(target_one));
-plot_range = r_pos-1000:r_pos+1000;
+plot_range = r_pos-Nr/2:r_pos+Nr/2;
 
 target_plot = abs(target_one(a_pos, plot_range));
 target_plot = (target_plot-min(target_plot))/(max(target_plot)-min(target_plot));
