3a6d89ac1a47517fd551ea4ccd441d117353f12d
wugfh <2759154365@qq.com>
2024-12-20 03:49:51 +0000

    complete classic bp

--- script/strip_mode/subband/sar_bp.py
+++ script/strip_mode/subband/sar_bp.py
@@ -58,14 +58,14 @@ class BpFocus:
         self.lambda_= self.c/self.f0
         self.theta_c = cp.arcsin(self.fc*self.lambda_/(2*self.Vr))
         self.La = 15
-        self.Ta = 0.8
+        self.Ta = 2
         self.Kr = -self.B/self.Tr
 
     def init_raw_data(self, data1):
         [Na, Nr] = cp.shape(data1)
         data = cp.zeros([int(cp.ceil(Na*1.5)), int(cp.ceil(Nr*1.5))], dtype=cp.complex128)
-        data[0:Na, 0:Nr] = data1
         [self.Na, self.Nr] = cp.shape(data)
+        data[self.Na/2-Na/2:self.Na/2+Na/2, self.Nr/2-Nr/2:self.Nr/2+Nr/2] = data1
         return data
 
     def echo_generate(self):
@@ -139,16 +139,22 @@ class BpFocus:
         mat_tau, mat_eta = cp.meshgrid(tau, eta)
         mat_R = mat_tau*self.c/2
         output = cp.zeros((self.Na, self.Nr), dtype=cp.complex128)
+        Tpulze_width = 0.886*self.lambda_*mat_R/(self.La*self.Vr*cp.cos(self.theta_c)**2)
 
         for i in tqdm(range(self.Na)):
+            ## eta_now是当前波束中心的位置
             eta_now = (i-self.Na/2)/self.PRF+eta_c
             R_eta = cp.sqrt(mat_R**2 + (self.Vr*(mat_eta-eta_now))**2)
             delta_t = 2*(R_eta-mat_R)/self.c
             delta = delta_t/(1/self.Fs)
+
+            ## 加上eta_c是为了对齐波束中心
+            Wa_width =  cp.abs(mat_eta-eta_now+eta_c) < Tpulze_width/2
             echo_pulse = cp.ones((self.Na, 1)) * cp.squeeze(echo[i,:])
             intp = self.sinc_interpolation(echo_pulse, delta, self.Na, self.Nr, 8)
             intp = intp*cp.exp(4j*cp.pi*R_eta/self.lambda_)
-            output = output + intp
+            output = output + intp*Wa_width
+        
         return output
     
 if __name__ == '__main__':
@@ -156,11 +162,11 @@ if __name__ == '__main__':
     data = sci.loadmat("../../../data/English_Bay_ships/data_1.mat")
     data = data['data_1']
     data = cp.array(data, dtype=cp.complex128)
-    # echo = bp.init_raw_data(data)
-    echo = bp.echo_generate()
-    plt.figure(1)
-    plt.imshow(cp.abs(echo).get(), aspect="auto")
-    plt.savefig("../../../fig/bp/echo.png", dpi=300)
+    echo = bp.init_raw_data(data)
+    # echo = bp.echo_generate()
+    # plt.figure(1)
+    # plt.imshow(cp.abs(echo).get(), aspect="auto")
+    # plt.savefig("../../../fig/bp/echo.png", dpi=300)
 
     echo_pre = bp.Bp_preprocess(echo)
     plt.figure(2)
@@ -171,6 +177,7 @@ if __name__ == '__main__':
     plt.savefig("../../../fig/bp/preprocess.png", dpi=300)
 
     output = bp.Bp_foucs(echo_pre)
+    output = cp.flip(output, axis=0)
     output = cp.abs(output)/cp.max(cp.max(cp.abs(output)))
     output = 20*cp.log10(output+1)
     output = output**0.4
