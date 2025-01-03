1961ae9edd53701ec68a1ebf6d295b9b777837d9
aw <2759154365@qq.com>
2025-01-03 20:30:25 +0800

    try to simulate cs in sparse sar data

---
+++ script/strip_mode/subband/cs_reconstruct.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cupy as cp             
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import scipy.io as sci
+
+class CS_Sim:
+    pass
+
+
+if __name__ == '__main__':
+    pass
