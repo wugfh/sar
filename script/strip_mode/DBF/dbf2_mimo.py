544afb21bd30d254dc9b014f2e2dcfe858257e10
wugfh <2759154365@qq.com>
2025-01-09 17:13:17 +0800

    complete dbf mimo first edtion

--- script/strip_mode/DBF/dbf2_mimo.py
+++ script/strip_mode/DBF/dbf2_mimo.py
@@ -41,7 +41,7 @@ class DBF_MIMO:
         mat_tau, mat_eta = cp.meshgrid(tau, eta)
         point_r = self.H/cp.cos(self.phi)+cp.linspace(-10000, 10000, 5)
         point_a = cp.linspace(-10000, 10000, 5)
-        echo = cp.zeros(self.azchan_N, self.Na, self.Nr)
+        echo = cp.zeros((self.azchan_N, self.Na, self.Nr), dtype=cp.complex128)
         for i in range(self.rchan_N):
             dr = self.dra*(i-(self.rchan_N-1)/2)
             for j in range(self.azchan_N):
@@ -50,12 +50,12 @@ class DBF_MIMO:
                     R_eta_tx = cp.sqrt(point_r[k]**2+(self.Vr*mat_eta - point_a[k])**2)
                     R_eta_rx = cp.sqrt(point_r[k]**2+(self.Vr*(mat_eta-da/self.Vr) - point_a[k])**2)
                     R_eta = (R_eta_rx+R_eta_tx)/2
-                    pulze_width = 0.886*(point_r[i]/cp.cos(self.theta_rc))*self.lambda_/(self.dza*self.Vr*cp.cos(self.theta_rc))
+                    pulze_width = 0.886*(point_r[k]/cp.cos(self.theta_rc))*self.lambda_/(self.dza*self.Vr*cp.cos(self.theta_rc))
                     Wa = cp.abs(mat_eta - eta_c - (point_a[k])/self.Vr) < pulze_width
 
                     # MIMO 模式下，发射端会同时发射n个不同信号
                     ## 发射信号1
-                    phi_tar = cp.arccos(point_r[i]/self.H)
+                    phi_tar = cp.arccos(point_r[k]/self.H)
                     mat_tau_ichan = mat_tau - da*cp.sin(phi_tar-self.phi)/self.c 
                     Wr = cp.abs(mat_tau_ichan - 2*R_eta/self.c) < self.Tp/2
                     Phase = cp.exp(1j*cp.pi*self.Kr*(mat_tau_ichan - 2*R_eta/self.c)**2)*cp.exp(-2j*cp.pi*self.fc*(2*R_eta/self.c))
@@ -71,9 +71,10 @@ class DBF_MIMO:
         return echo
 
     def range_compress(self, echo):
-        f_tau = cp.fft.fftshift(cp.arange(-self.Nr/2, self.Nr/2)*(self.Fr/self.Nr))
-        echo_f_rc = cp.zeros((self.Na, self.Nr, self.azchan_N))
-        mat_f_tau = cp.ones(self.Na,1) @ f_tau
+        f_tau = cp.fft.fftshift(cp.arange(-self.Nr/2, self.Nr/2)*(self.Fr/self.Nr))[cp.newaxis, :]
+        echo_f_rc = cp.zeros((self.Na, self.Nr, self.azchan_N), dtype=cp.complex128)
+
+        mat_f_tau = cp.ones((self.Na,1)) @ f_tau
         Hr = cp.exp(1j*cp.pi*mat_f_tau**2/self.Kr)
         for i in range(self.azchan_N):
             echo_chan = cp.squeeze(echo[i, : ,:])
@@ -96,10 +97,10 @@ class DBF_MIMO:
         return P_matrix
     
     def rd_rcmc(self, echo_f_rc):
-        tau = 2*self.Rc/self.c + cp.arange(-self.Nr/2, self.Nr/2, 1)*(1/self.Fr)
-        mat_tau = cp.ones(self.Na_up ,1) @ tau
-        f_eta_up = 2*self.Vr*cp.sin(self.theta_rc)/self.lambda_ + cp.fft.fftshift(cp.arange(-self.Na_up/2, self.Na_up/2)*(self.PRF/self.Na))
-        mat_f_eta = cp.transpose(f_eta_up) @ cp.ones(1, self.Nr)
+        tau = (2*self.Rc/self.c + cp.arange(-self.Nr/2, self.Nr/2, 1)*(1/self.Fr))[:, cp.newaxis]
+        mat_tau = cp.ones((self.Na_up ,1)) @ cp.transpose(tau)
+        f_eta_up = 2*self.Vr*cp.sin(self.theta_rc)/self.lambda_ + cp.fft.fftshift(cp.arange(-self.Na_up/2, self.Na_up/2)*(self.PRF/self.Na))[:, cp.newaxis]
+        mat_f_eta = (f_eta_up) @ cp.ones((1, self.Nr))
         mat_R = mat_tau*self.c/2
         delta_R = self.lambda_**2 * mat_f_eta**2 * mat_R/(8*self.Vr)
         delta = (delta_R*2/self.c)/(1/self.Fr)
@@ -112,13 +113,13 @@ class DBF_MIMO:
         self.Na_up = self.Na*self.azchan_N
         out_band = cp.zeros((self.azchan_N, self.Na, self.Nr), dtype=cp.complex128) 
         for i in range(self.Na):
-            aperture = cp.squeeze(echo_f_rc[:, :, i])
+            aperture = cp.squeeze(echo_f_rc[i, :, :])
             P_aperture = cp.squeeze(P_matrix[i, :, :])
             tmp = aperture @ P_aperture
             for j in range(self.azchan_N):
                 out_band[j, i, :] = tmp[:, j]
 
-        echo_f_rc  = cp.zeros(self.Na_up, self.Nr)        
+        echo_f_rc  = cp.zeros((self.Na_up, self.Nr), dtype=cp.complex128)        
         for j in range(self.azchan_N):
             echo_f_rc[j * self.Na: (j + 1) * self.Na, :] = cp.fft.fftshift(cp.squeeze(out_band[j, :, :]), axes=0)
 
@@ -126,12 +127,12 @@ class DBF_MIMO:
         
     def azimuth_compress(self, echo_f_rc):
         echo_f_rc = self.rd_rcmc(echo_f_rc)
-        tau = 2*self.Rc/self.c + cp.arange(-self.Nr/2, self.Nr/2, 1)*(1/self.Fr)
-        mat_tau = cp.ones(self.Na_up ,1) @ tau
+        tau = 2*self.Rc/self.c + (cp.arange(-self.Nr/2, self.Nr/2, 1)*(1/self.Fr))[cp.newaxis, :]
+        mat_tau = cp.ones((self.Na_up ,1)) @ tau
         mat_R = mat_tau*self.c/2
         Ka = 2 * self.Vr**2 * cp.cos(self.theta_rc)**2 / (self.lambda_ * mat_R)
-        f_eta_up = 2*self.Vr*cp.sin(self.theta_rc)/self.lambda_ + cp.fft.fftshift(cp.arange(-self.Na_up/2, self.Na_up/2)*(self.PRF/self.Na))
-        mat_f_eta = cp.transpose(f_eta_up) @ cp.ones(1, self.Nr)
+        f_eta_up = 2*self.Vr*cp.sin(self.theta_rc)/self.lambda_ + cp.fft.fftshift(cp.arange(-self.Na_up/2, self.Na_up/2)*(self.PRF/self.Na))[:, cp.newaxis]
+        mat_f_eta = (f_eta_up) @ cp.ones((1, self.Nr))
         Ha = cp.exp(-1j*cp.pi*mat_f_eta**2/Ka)
         out = cp.fft.ifft(Ha*echo_f_rc, self.Na_up, axis=0)
 
@@ -140,6 +141,7 @@ class DBF_MIMO:
 
 
 if __name__ == '__main__':
+    cp.cuda.Device(1).use()
     fc = 9.6e9
     H = 700e3
     Br = 100e6
@@ -158,5 +160,15 @@ if __name__ == '__main__':
     echo_f_rc = dbf_mimo.range_compress(echo)
     echo_f_rc = dbf_mimo.azimuth_reconstruct(echo_f_rc)
     out = dbf_mimo.azimuth_compress(echo_f_rc)
+    
+    plt.figure(figsize = (8,8))
+    plt.imshow((cp.abs(echo_f_rc)).get(), cmap='jet', aspect='auto')
+    plt.colorbar()
+    plt.savefig("../../../fig/dbf/dbf_mimo_rd.png")
+
+    plt.figure(figsize = (8,8))
+    plt.imshow((cp.abs(out)).get(), cmap='jet', aspect='auto')
+    plt.colorbar()
+    plt.savefig("../../../fig/dbf/dbf_mimo_out.png")
 
 
