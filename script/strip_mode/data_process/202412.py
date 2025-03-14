5856e924c3224c5590e0932a94286c4ffa88e012
aw <2759154365@qq.com>
2025-03-14 16:07:29 +0800

    sync

--- script/strip_mode/data_process/202412.py
+++ script/strip_mode/data_process/202412.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(r"../")
 from sar_focus import SAR_Focus
 
-cp.cuda.Device(2).use()
+cp.cuda.Device(0).use()
 
 class Fcous_Air:
     def __init__(self, Tr, Br, f0, t0, Fr, PRF, fc, Vr):
@@ -19,16 +19,17 @@ class Fcous_Air:
         self.Fr = Fr
         self.PRF = PRF
         self.fc = fc
-        self.Vr = 72.2475
+        self.Vr = Vr
         self.lambda_ = self.c/self.f0
-        self.R0 = (t0-Tr/2)*self.c/2
+        self.R0 = (t0)*self.c/2
         self.Kr = self.Br/self.Tr
-        self.focus = SAR_Focus(Fr, Tr, f0, PRF, self.Vr, Br, fc, self.R0, self.Kr)
+        self.focus = SAR_Focus(Fr, Tr, f0, PRF, Vr, Br, fc, self.R0, self.Kr)
         
     def read_data(self, data_filename, pos_filename):
         with h5py.File(data_filename, "r") as data:
             sig = data['sig']
             sig = sig["real"] + 1j*sig["imag"]
+            sig = np.array(sig)
 
         with h5py.File(pos_filename) as pos:    
             self.forward = np.squeeze(np.array(pos['forward']))
@@ -36,9 +37,11 @@ class Fcous_Air:
             self.down = np.squeeze(np.array(pos['down']))
             self.frame_time = self.time2sec(np.array(pos['frame_time']))
 
+        print(sig.shape)
+        self.Na, self.Nr = np.shape(sig)
         self.sig = np.array(sig)
-        self.sig = np.transpose(self.sig)
         self.Na, self.Nr = np.shape(self.sig)
+        print(self.Na, self.Nr)
     
     def inverse_rc(self):
         f_tau = np.fft.fftshift(np.linspace(-self.Nr/2,self.Nr/2-1,self.Nr)*(self.Fr/self.Nr))
@@ -81,10 +84,11 @@ class Fcous_Air:
     
 
 if __name__ == '__main__':
-    # focus_air = Fcous_Air(24e-6, 2e9, 3.5e10, 3.46e-5, 2.5e9, 5000.1, 0, 72.2475)
-    focus_air = Fcous_Air(4.175000000000000e-05, 30.111e+06 , 5.300000000000000e+09 ,  6.5959e-03, 32317000, 1.256980000000000e+03, -6900, 7062)
-    # focus_air.read_data("../../../data/example_49_cropped_sig_rc_small.mat", "../../../data/pos.mat")
-    focus_air.read_data("../../../data/English_ship.mat", "../../../data/data_202412/pos.mat")
+    focus_air = Fcous_Air(24e-6, 2e9, 3.5e10, 3.46e-5, 2.5e9, 5000.1, 0, 72.2475)
+    # focus_air = Fcous_Air(4.175000000000000e-05, 30.111e+06 , 5.300000000000000e+09 ,  6.5959e-03, 32317000, 1.256980000000000e+03, -6900, 7062)
+    focus_air.read_data("../../../data/security/202412/example_49_cropped_sig_rc_small.mat", "../../../data/security/202412/pos.mat")
+
+    # focus_air.read_data("./English_Bay_ships.mat", "./pos.mat")
 
     print(np.mean(np.diff(focus_air.forward)/np.diff(focus_air.frame_time)))
     print(np.mean(np.diff(focus_air.right)/np.diff(focus_air.frame_time)))
@@ -94,12 +98,14 @@ if __name__ == '__main__':
     plt.imshow(np.abs(focus_air.sig), cmap='gray', aspect='auto')
     plt.tight_layout()
     plt.savefig("../../../fig/data_202412/echo.png")
+
+    focus_air.inverse_rc()
     
     image = focus_air.focus.wk_focus(cp.array(focus_air.sig), focus_air.R0)
     image_abs = cp.abs(image)
     image_abs = image_abs/cp.max(cp.max(image_abs))
     image_abs = 20*cp.log10(image_abs+1)
-    image_abs = image_abs**0.4
+    image_abs = image_abs**0.3
     plt.figure()
     plt.imshow(image_abs.get(), cmap='gray', aspect='auto')
     plt.tight_layout()
