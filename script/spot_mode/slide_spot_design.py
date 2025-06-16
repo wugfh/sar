6e81a93f9708d4a59439e40ae57698dbbe5c17b8
wugfh <2759154365@qq.com>
2025-06-16 18:43:57 +0800

    low orbit version 4

--- script/spot_mode/slide_spot_design.py
+++ script/spot_mode/slide_spot_design.py
@@ -73,8 +73,8 @@ class SlideSpotDesign:
         self.psi_end = self.psi_0 + self.omega * self.Ta/2
         self.theta_a = self.lambda_ * np.cos(self.psi_0)/(2*self.da/self.A)
         self.theta_c = 0
-        self.La = np.min(0.886*self.lambda_/self.theta_a)
-        self.Lr = np.min(0.886*self.lambda_/(self.look_angle_right-self.look_angle_left))
+        self.La = np.max(0.886*self.lambda_/self.theta_a)
+        self.Lr = np.max(0.886*self.lambda_/(self.look_angle_right-self.look_angle_left))
         self.La = np.floor(self.La*10)/10
         self.Lr = np.floor(self.Lr*10)/10
         # self.Lr = 0.75
